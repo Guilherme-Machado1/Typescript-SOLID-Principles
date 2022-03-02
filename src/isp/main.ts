68c18c7d1431ca881ad4444a0e96c4540ccf01eb
Guilherme Santos <guilhermemachado1900@gmail.com>
2022-03-02 15:35:10 -0300

    Main file

---
+++ src/isp/main.ts
@@ -0,0 +1,36 @@
+/*
+Interface Segregation Principle - Os Cliente não devem ser forçados a depender de types, interfaces, ou membros da classe abstrata que não utilizam.
+*/
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { TenPercentDiscount } from './classes/discount';
+import { Customer, CompanyCustomer } from './classes/customer';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+const tenPercentDiscount = new TenPercentDiscount();
+// const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(tenPercentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const individualCustomer = new Customer('Teste', 'Teste', '329.324.043-32');
+const companyCustomer = new CompanyCustomer('Teste', '329.324.043-32');
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  individualCustomer,
+);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDicount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
